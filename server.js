ee4e727bb09f178211d44747ced3bda30a30d37b
vladarte <52295760+vladarte@users.noreply.github.com>
2020-03-28 11:13:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: [''],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
